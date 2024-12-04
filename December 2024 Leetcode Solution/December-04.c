5ab269375f7ed15c95e952884f59d1d751ff263a
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-04 20:09:47 +0530

    Create December-04.c

---
+++ December 2024 Leetcode Solution/December-04.c
@@ -0,0 +1,9 @@
+bool canMakeSubsequence(char* str1, char* str2) {
+    int j = 0;
+    for (int i = 0; str1[i] != '\0' && str2[j] != '\0'; ++i) {
+        if (str1[i] == str2[j] || (str1[i] - 'a' + 1) % 26 + 'a' == str2[j]) {
+            ++j;
+        }
+    }
+    return str2[j] == '\0';
+}
